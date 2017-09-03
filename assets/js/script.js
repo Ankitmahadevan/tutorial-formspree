0983ecffbb7cc73cbefdd17b541a0bb402a5ed2d
Ankit Yadav <161210010@nitdelhi.ac.in>
2017-09-03 22:33:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(function($){
 		event.preventDefault();
 
 		$.ajax({
-	    url: "http://formspree.io/ankit.kumar.yadav0001@gmail.com", 
+	    url: "", 
 	    method: "POST",
 	    data: {
 	    	name: $("#name").val(),
